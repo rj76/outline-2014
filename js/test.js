45a89eb86306c195d389331b0c7e02214b66f110
Richard Jansen <humanications@gmail.com>
2014-05-15 14:26:27 +0200

    Working room with sprite that moves from door to door

--- js/test.js
+++ js/test.js
@@ -1,14 +1,33 @@
 (function ($) {
-//    var canvas = document.getElementById("intro_canv"),
-//        canvas_hidden = document.getElementById("intro_canv_hidden"),
-//        ctx = canvas.getContext("2d"),
-//        ctx_hidden = canvas_hidden.getContext("2d"),
-//        w = canvas.width,
-//        h = canvas.height
-//    ;
+    var canvas = document.getElementById("canv"),
+        canvas_hidden = document.getElementById("canv_hidden"),
+        ctx = canvas.getContext("2d"),
+        ctx_hidden = canvas_hidden.getContext("2d"),
+        w = canvas.width,
+        h = canvas.height
+    ;
 
-//    ctx.fillStyle = '#fff'; // set canvas background color
-//    ctx.fillRect(0, 0, w, h);  // now fill the canvas
+    ctx.fillStyle = '#fff';
+    ctx.fillRect(0, 0, w, h);
+    ctx.globalCompositeOperation = 'source-over';
+
+    /*
+        het moet mogelijk zijn op te geven welke kant de spy op gaat,
+        tot welke positie en welke sprite (kant dan op).
+        Dus de richtingen moeten voor geprogrammeerd worden, waarbij de spy
+        van start_x naar end_x beweegt
+
+     */
+
+//    var sprite = new $.sprite();
+//    sprite.start({
+//        ctx: ctx,
+//        w: w,
+//        h: h,
+//        s_w: 32,
+//        s_h: 37,
+//        img: 'img/walk-right.png'
+//    });
 
     var room = new $.room();
     room.start();
--- js/visuals/jquery.room.js
+++ js/visuals/jquery.room.js
@@ -3,9 +3,10 @@
         ctx = canvas.getContext("2d"),
         w = canvas.width,
         h = canvas.height,
+        base_img,
         zoom=50,
         top=50,
-        right=50,
+        offset_left=50,
         line_width=2,
         colors = ['#d17ded', '#f9d99c'],
         coords_fill = [
@@ -27,16 +28,191 @@
             { type: 'lineto', x: 8, y: 0 },
             { type: 'moveto', x: 8, y: 2.5 },
             { type: 'lineto', x: 10, y: 5 }
-        ]
+        ],
+        doors = {
+            left: {
+                coords: [
+                    { x: 0.8, y: 4 },
+                    { x: 1.2, y: 3.5 },
+                    { x: 1.2, y: 1.6 },
+                    { x: 0.8, y: 2 }
+                ],
+                sprite: {
+                    x: 0.8,
+                    y: 3
+                }
+            },
+            right: {
+               coords: [
+                    { x: 10-0.8, y: 4 },
+                    { x: 10-1.2, y: 3.5 },
+                    { x: 10-1.2, y: 1.6 },
+                    { x: 10-0.8, y: 2 }
+                ],
+                sprite: {
+                    x: 10-0.8,
+                    y: 4
+                }
+            },
+            back_left: {
+                coords: [
+                    { x: 3, y: 2.5 },
+                    { x: 4, y: 2.5 },
+                    { x: 4, y: .8 },
+                    { x: 3, y: .8 }
+                ],
+                sprite: {
+                    x: 3,
+                    y: 2.5
+                }
+            },
+            back_right: {
+                coords: [
+                    { x: 6, y: 2.5 },
+                    { x: 7, y: 2.5 },
+                    { x: 7, y: .8 },
+                    { x: 6, y: .8 }
+                ],
+                sprite: {
+                    x: 6.2,
+                    y: 1
+                }
+            }
+        },
+        sprites = {
+            spy: {
+                speed: 1,
+                zoom: 1.3,
+                time: 0.2,
+                wait: 10,
+                num_images: 0,
+                num_objects: 0,
+                punch_right: {
+                    s_w: 28,
+                    s_h: 37,
+                    img: 'img/sprites/punch-right.png'
+                },
+                walk_left: {
+                    s_w: 32,
+                    s_h: 37,
+                    img: 'img/sprites/walk-left.png'
+                },
+                walk_right: {
+                    s_w: 32,
+                    s_h: 37,
+                    img: 'img/sprites/walk-right.png'
+                },
+                walk_bw: {
+                    s_w: 25,
+                    s_h: 36,
+                    img: 'img/sprites/walk-bw.png'
+                },
+                walk_fw: {
+                    s_w: 25,
+                    s_h: 36,
+                    img: 'img/sprites/walk-fw.png'
+                }
+            }
+        },
+        wallpapers = [
+            'img/wall/alpacaneck.jpg',
+            'img/wall/knien.jpg',
+            'img/wall/mandy.jpg',
+            'img/wall/patrick.jpg',
+            'img/wall/pony.png',
+            'img/wall/trolvis.jpg',
+            'img/wall/ghunter.jpg',
+            'img/wall/alpacafluffy.jpg'
+        ],
+        wallpaper_objects = [],
+        room_configs = ['lb', 'lr', 'rb'], // door: left and back (lb), left and right (lr), right and back (rb)
+        active_config,
+        tune_idx=0
     ;
+
     $.room = function() {};
     $.room.prototype.start = function () {
-        createRoom({
-            color: colors[0]
+        // load images
+        $.when(loadWallpapers(), loadSprites()).then(function() {
+            console.log('all images loaded');
+            Modernizr.load({
+                load: [
+                    'js/sprite/Animation.js',
+                    'js/sprite/FrameTimer.js',
+                    'js/sprite/SpriteSheet.js'
+                ],
+                complete: function() {
+                    for(tune_idx=0;tune_idx<3;tune_idx++) {
+                        // clear and fade in
+
+                        // create room config
+                        active_config = room_configs[randomIntFromInterval(0, room_configs.length-1)];
+                        active_config = 'lb';
+
+                        createRoom();
+                        createDoors();
+
+                        switch(active_config) {
+                            case 'lb':
+                                createPainting(2.5*zoom);
+                                break;
+                            case 'lr':
+                                createPainting(2.5*zoom);
+                                createPainting(5.5*zoom);
+                                break;
+
+                            case 'rb':
+                                createPainting(5.5*zoom);
+                                break;
+                        }
+
+                        base_img = ctx.getImageData(0, 0, w, h);
+
+                        // move sprite through room
+                        $.when(moveSpy()).then(doVisual);
+
+                        // start visual
+
+                        // fade out
+
+                    }
+                }
+            });
         });
     };
 
-    function createRoom(opts) {
+    function loadWallpapers() {
+        var d = new $.Deferred();
+        for(var i=0;i<wallpapers.length;i++) {
+            var img = new Image();
+            img.onload = function() {
+                if (wallpaper_objects.length==wallpapers.length) d.resolve();
+            };
+            img.src = wallpapers[i];
+            wallpaper_objects.push(img);
+        }
+        return d;
+    }
+
+    function loadSprites() {
+        var d = new $.Deferred(), a = [], name;
+        for(name in sprites.spy) if (typeof sprites.spy[name].img != 'undefined') a.push(sprites.spy[name].img);
+        sprites.spy.num_images = a.length;
+        for(name in sprites.spy) {
+            if (typeof sprites.spy[name].img == 'undefined') continue;
+            var img = new Image();
+            img.onload = function() {
+                sprites.spy.num_objects++;
+                if (sprites.spy.num_objects==sprites.spy.num_images) d.resolve();
+            };
+            img.src = sprites.spy[name].img;
+
+            sprites.spy[name].object = img;
+        }
+        return d;
+    }
+
+    function createRoom() {
         ctx.clearRect(0, 0, w, h);
         ctx.fillStyle = '#fff';
         ctx.fillRect(0, 0, w, h);
@@ -44,29 +220,297 @@
         ctx.lineWidth = line_width;
 
         ctx.strokeStyle = '#000';
-        ctx.fillStyle = opts.color;
+        ctx.fillStyle = colors[randomIntFromInterval(0, colors.length-1)];
         ctx.beginPath();
         moveOrDrawCoords(coords_fill);
         moveOrDrawCoords(coords_innerlines);
         ctx.fill();
         ctx.stroke();
-
     }
 
     function moveOrDrawCoords(coords) {
         for (var p=0;p<coords.length;p++) {
             if(coords[p].type == 'moveto') {
-                ctx.moveTo(coords[p].x*zoom+right, coords[p].y*zoom+right);
+                ctx.moveTo(coords[p].x*zoom+offset_left, coords[p].y*zoom+offset_left);
             } else {
-                ctx.lineTo(coords[p].x*zoom+right, coords[p].y*zoom+top);
+                ctx.lineTo(coords[p].x*zoom+offset_left, coords[p].y*zoom+top);
             }
         }
     }
 
-    function createDoor(opts) {
-        var left_coords = [
-            { x: 0 }
-        ]
+    function randomIntFromInterval(min, max) {
+        return Math.floor(Math.random()*(max-min+1)+min);
+    }
+
+    function createPainting(offset_x) {
+        /*
+            TODO: verticaal centreren, en horizontaal centreren checken
+                    portrait implementeren
+         */
+        var
+            idx = Math.floor(Math.random()*(wallpaper_objects.length-1)+1),
+            img = wallpaper_objects[idx],
+            mode = img.height > img.width ? 'p' : 'l',
+            offset_y = .2*zoom,
+            width_x_min=1*zoom,
+            width_x_max=1.5*zoom,
+            height_y_min=.8*zoom,
+            height_y_max=1.2*zoom,
+            ratio, img_x, img_y
+        ;
+
+        if (mode == 'l') {
+            // width can't be bigger than 2*zoom
+            // pick ramdon width between 2*zoom and 1.5*zoom
+            // adjust height to this factor and place image in center
+            //
+            var
+                new_width = randomIntFromInterval(width_x_min, width_x_max)
+            ;
+            ratio = new_width/img.width;
+            img_x = offset_left+offset_x+(width_x_max/2-new_width/2);
+            img_y = offset_y+(4.5*zoom)/2-(img.height*ratio)/2;
+            ctx.drawImage(img, img_x, img_y, img.width*ratio, img.height*ratio);
+        }
+
+        if (mode == 'p') {
+            // height can't be bigger than 1.5*zoom
+            var
+                new_height = randomIntFromInterval(height_y_min, height_y_max)
+            ;
+            ratio = new_height/img.height;
+            img_y = offset_y+1.5*zoom+(height_y_max/2-new_height/2);
+            img_x = offset_left+offset_x+((img.width*ratio)/2);
+            ctx.drawImage(img, img_x, img_y, img.width*ratio, img.height*ratio);
+        }
+
+        ctx.lineWidth = 2;
+        ctx.beginPath();
+        ctx.moveTo(img_x, img_y);
+        ctx.lineTo(img_x+img.width*ratio, img_y);
+        ctx.lineTo(img_x+img.width*ratio, img_y+img.height*ratio);
+        ctx.lineTo(img_x, img_y+img.height*ratio);
+        ctx.lineTo(img_x, img_y);
+        ctx.stroke();
+
+        ctx.lineWidth = 1;
+        ctx.moveTo(img_x+(img.width*ratio)/2, img_y-.5*zoom);
+        ctx.lineTo(img_x+(img.width*ratio)/3, img_y);
+        ctx.moveTo(img_x+(img.width*ratio)/2, img_y -.5*zoom);
+        ctx.lineTo(img_x+img.width*ratio-(img.width*ratio)/3, img_y);
+        ctx.stroke();
+    }
+
+    function createDoors() {
+        ctx.fillStyle = '#000';
+
+        if (active_config=='lb' || active_config=='lr') {
+            ctx.beginPath();
+            ctx.moveTo(doors.left.coords[0].x*zoom+offset_left, doors.left.coords[0].y*zoom+offset_left);
+            for(var i=0;i<doors.left.coords.length;i++) {
+                ctx.lineTo(doors.left.coords[i].x*zoom+offset_left, doors.left.coords[i].y*zoom+top);
+            }
+            ctx.fill();
+        }
+
+        if (active_config=='rb' || active_config=='lr') {
+            ctx.beginPath();
+            ctx.moveTo(doors.right.coords[0].x*zoom+offset_left, doors.right.coords[0].y*zoom+offset_left);
+            for(var i=0;i<doors.right.coords.length;i++) {
+                ctx.lineTo(doors.right.coords[i].x*zoom+offset_left, doors.right.coords[i].y*zoom+top);
+            }
+            ctx.fill();
+        }
+
+        if (active_config=='rb') {
+            ctx.beginPath();
+            ctx.moveTo(doors.back_left.coords[0].x*zoom+offset_left, doors.back_left.coords[0].y*zoom+offset_left);
+            for(var i=0;i<doors.back_left.coords.length;i++) {
+                ctx.lineTo(doors.back_left.coords[i].x*zoom+offset_left, doors.back_left.coords[i].y*zoom+top);
+            }
+            ctx.fill();
+        }
+
+        if (active_config=='lb') {
+            ctx.beginPath();
+            ctx.moveTo(doors.back_right.coords[0].x*zoom+offset_left, doors.back_right.coords[0].y*zoom+offset_left);
+            for(var i=0;i<doors.back_right.coords.length;i++) {
+                ctx.lineTo(doors.back_right.coords[i].x*zoom+offset_left, doors.back_right.coords[i].y*zoom+top);
+            }
+            ctx.fill();
+        }
+    }
+
+    function moveSpy() {
+        /*
+            TODO:
+            - make image bigger? met factor
+            - move from door to door
+         */
+        switch(active_config) {
+            case 'lr':
+                spyLeftRight();
+                break;
+            case 'rb':
+                spyRightBack();
+                break;
+            case 'lb':
+                var x = doors.left.sprite.x*zoom+offset_left;
+                var y = top+doors.left.sprite.y*zoom;
+                var end_x = offset_left+doors.back_right.sprite.x*zoom;
+                $.when(moveSpyRight({
+                        x: x,
+                        end_x: end_x,
+                        y: y
+                    }))
+                    .then(function() {
+                        moveSpyBack({
+                            x: end_x,
+                            end_y: top+doors.back_right.sprite.y*zoom,
+                            y: y
+                        });
+                    });
+                break;
+        }
+    }
+
+    function moveSpyRight(opts) {
+        // move to right: x from doors.left.x/y to doors.back_right.x
+        var
+            d = new $.Deferred(),
+            timer = new FrameTimer(),
+            spritesheet = new SpriteSheet({
+                width: sprites.spy.walk_right.s_w,
+                height: sprites.spy.walk_right.s_h,
+                sprites: [
+                    { name: 'fr_1', x: 0, y: 0 },
+                    { name: 'fr_2', x: 0, y: 0 }
+                ]
+            }),
+            animation = new Animation([
+                    { sprite: 'fr_1', time: sprites.spy.time },
+                    { sprite: 'fr_2', time: sprites.spy.time }
+            ], spritesheet)
+        ;
+
+        var t = setInterval(function() {
+            opts.x += sprites.spy.speed;//, console.log(x)
+            if (opts.x>=opts.end_x) {
+                clearTimeout(t);
+                d.resolve();
+                return;
+            }
+
+            animation.animate(timer.getSeconds());
+            var frame = animation.getSprite();
+            ctx.clearRect(0, 0, w, h);
+            ctx.putImageData(base_img, 0, 0);
+            ctx.drawImage(
+                sprites.spy.walk_right.object,
+                frame.x, frame.y,
+                sprites.spy.walk_right.s_w, sprites.spy.walk_right.s_h,
+                opts.x, opts.y,
+                sprites.spy.walk_right.s_w, sprites.spy.walk_right.s_h
+            );
+
+            timer.tick();
+        }, sprites.spy.wait);
+
+        return d;
+    }
+
+    function moveSpyBack(opts) {
+        // move to top: y from doors.left.y to doors.back_right.y
+        var
+            d = new $.Deferred(),
+            timer = new FrameTimer(),
+            spritesheet = new SpriteSheet({
+                width: sprites.spy.walk_bw.s_w,
+                height: sprites.spy.walk_bw.s_h,
+                sprites: [
+                    { name: 'fr_1', x: 0, y: 0 },
+                    { name: 'fr_2', x: 0, y: 0 }
+                ]
+            }),
+            animation = new Animation([
+                { sprite: 'fr_1', time: sprites.spy.time },
+                { sprite: 'fr_2', time: sprites.spy.time }
+            ], spritesheet)
+        ;
+
+        var t = setInterval(function() {
+            opts.y -= sprites.spy.speed;
+            if (opts.y<=opts.end_y) {
+                clearTimeout(t);
+                d.resolve();
+                return;
+            }
+
+            animation.animate(timer.getSeconds());
+            var frame = animation.getSprite();
+            ctx.clearRect(0, 0, w, h);
+            ctx.putImageData(base_img, 0, 0);
+            ctx.drawImage(
+                sprites.spy.walk_bw.object,
+                frame.x, frame.y,
+                sprites.spy.walk_bw.s_w, sprites.spy.walk_bw.s_h,
+                opts.x, opts.y,
+                sprites.spy.walk_bw.s_w, sprites.spy.walk_bw.s_h
+            );
+
+            timer.tick();
+        }, sprites.spy.wait);
+
+        return d;
+    }
+
+    function moveSpyLeft(opts) {
+        // move to right: x from doors.left.x/y to doors.back_right.x
+        var
+            d = new $.Deferred(),
+            timer = new FrameTimer(),
+            spritesheet = new SpriteSheet({
+                width: sprites.spy.walk_left.s_w,
+                height: sprites.spy.walk_left.s_h,
+                sprites: [
+                    { name: 'fr_1', x: 0, y: 0 },
+                    { name: 'fr_2', x: 0, y: 0 }
+                ]
+            }),
+            animation = new Animation([
+                    { sprite: 'fr_1', time: sprites.spy.time },
+                    { sprite: 'fr_2', time: sprites.spy.time }
+            ], spritesheet)
+        ;
+
+        var t = setInterval(function() {
+            opts.x -= sprites.spy.speed;//, console.log(x)
+            if (opts.x<=opts.end_x) {
+                clearTimeout(t);
+                d.resolve();
+                return;
+            }
+
+            animation.animate(timer.getSeconds());
+            var frame = animation.getSprite();
+            ctx.clearRect(0, 0, w, h);
+            ctx.putImageData(base_img, 0, 0);
+            ctx.drawImage(
+                sprites.spy.walk_left.object,
+                frame.x, frame.y,
+                sprites.spy.walk_left.s_w, sprites.spy.walk_left.s_h,
+                opts.x, opts.y,
+                sprites.spy.walk_left.s_w, sprites.spy.walk_left.s_h
+            );
+
+            timer.tick();
+        }, sprites.spy.wait);
+
+        return d;
+    }
+
+    function doVisual() {
+
     }
 
 })(jQuery);
